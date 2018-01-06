97fa0084eeb3accde4ae95ec635a9f6d476bd9d3
gilbeckers <gil.beckers@hotmail.com>
2018-01-06 18:54:19 +0100

    implementing interference() 4

--- src/networks.py
+++ src/networks.py
@@ -15,7 +15,8 @@ def _get_base_path():
 def get_network(type, placeholder_input, sess_for_load=None):
     if type == 'mobilenet':
         net = MobilenetNetwork({'image': placeholder_input}, conv_width=0.75, conv_width2=0.50)
-        pretrain_path = 'trained/model-388003'
+        #pretrain_path = 'pretrained/mobilenet_v1_0.75_224_2017_06_14/mobilenet_v1_0.75_224.ckpt'
+        pretrain_path = 'pretrained/mobilenet_release/model-388003'
         last_layer = 'MConv_Stage6_L{aux}_5'
     elif type == 'mobilenet_accurate':
         net = MobilenetNetwork({'image': placeholder_input}, conv_width=1.00)
@@ -39,7 +40,7 @@ def get_network(type, placeholder_input, sess_for_load=None):
             s = '%dx%d' % (placeholder_input.shape[2], placeholder_input.shape[1])
             ckpts = {
                 #'mobilenet': 'trained/mobilenet_%s/model-release' % s,
-                'mobilenet': 'trained/model-388003',
+                'mobilenet': 'pretrained/mobilenet_release/model-388003',
                 'mobilenet_fast': 'trained/mobilenet_fast/model-163000',
                 'mobilenet_accurate': 'trained/mobilenet_accurate/model-170000'
             }
